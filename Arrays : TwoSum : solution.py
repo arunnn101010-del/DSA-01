e8db5f409884696dd98e3760f550e2d3894302ab
Arun <arunnn101010@gmail.com>
2026-03-31 23:19:18 +0530

    Add Two Sum solution using hashmap approach
    Implemented the Two Sum problem using a hashmap for optimized performance.

--- Arrays : TwoSum : solution.py
+++ Arrays : TwoSum : solution.py
@@ -1,1 +1,23 @@
+# Pomblem - Two Sum 
+# Approach : Hashmap Approach (optimized appraoch)
+# Time complexity and Space complexity : 0(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int tar) {
+        unordered_map<int, int> m;
+        vector<int> ans;
 
+         for(int i=0; i<arr.size(); i++) {
+            int first = arr[i];
+            int sec = tar - first;
+
+            if(m.find(sec) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
+            }
+            m[first] = i;
+        }   
+        return ans;
+    }
+};
