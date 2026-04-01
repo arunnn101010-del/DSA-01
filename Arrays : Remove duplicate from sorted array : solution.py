3959229a5f5ca4a7f324acbc07179b60b85f7d7c
Arun <arunnn101010@gmail.com>
2026-04-01 23:32:52 +0530

    Add solution to remove duplicates from sorted array
    Implemented a two-pointer approach to remove duplicates from a sorted array.

--- Arrays : Remove duplicate from sorted array : solution.py
+++ Arrays : Remove duplicate from sorted array : solution.py
@@ -1,1 +1,17 @@
+# Promblem - Remove duplicate from sorted array 
+# Approach - 2 pointer 
+# Time and space complexity - O(n) & O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 1;
 
+        for(int j=1; j< nums.size(); j++) {
+            if(nums[j] != nums[i-1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};
