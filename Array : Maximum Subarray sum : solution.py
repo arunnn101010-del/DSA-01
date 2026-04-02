7720641a529c8b8fab5eaff647e5baa53a3b42ac
Arun <arunnn101010@gmail.com>
2026-04-02 23:54:17 +0530

    Add brute force solution for maximum subarray sum
    Implemented the brute force approach for finding the maximum subarray sum with time complexity O(n^2).

--- Array : Maximum Subarray sum : solution.py
+++ Array : Maximum Subarray sum : solution.py
@@ -1,1 +1,20 @@
+# promblem - Maximum Subarray sum 
+# Apporach - Brute force approach 
+# time and space complexity - O(n^2) & O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum  = INT_MIN;
+        int currSum = 0;
 
+        for(int i=0; i<nums.size(); i++) {
+            currSum += nums[i];
+            maxSum = max(maxSum, currSum);
+
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
