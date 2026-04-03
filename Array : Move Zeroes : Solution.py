42f91f135341da443b90f8915fc202e851d37d6a
Arun <arunnn101010@gmail.com>
2026-04-03 16:48:52 +0530

    Add moveZeroes function to shift zeroes in array
    Implemented an optimal approach to move zeroes in an array, maintaining the order of non-zero elements.

--- Array : Move Zeroes : Solution.py
+++ Array : Move Zeroes : Solution.py
@@ -1,1 +1,17 @@
+# Promblem - Move Zeroes 
+# Approach - Optimal Approach 
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 283
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
 
+        for(int j = 0; j < nums.size(); j++) {
+            if(nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+    }
+};
