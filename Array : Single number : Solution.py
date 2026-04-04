49ec2fb9019d49b8e504ef2230f094b2d0b7ec1e
Arun <arunnn101010@gmail.com>
2026-04-04 15:12:21 +0530

    Add solution for Single Number problem
    Implement solution for finding single number using bit manipulation.

--- Array : Single number : Solution.py
+++ Array : Single number : Solution.py
@@ -1,1 +1,15 @@
+#  promblem - Single number
+# Approach - Bit manipulation
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 136
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
 
+        for(int i = 0; i < nums.size(); i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
