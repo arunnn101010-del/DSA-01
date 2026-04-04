351a25c58c7c720ca24e539b6c8f37d3330ed740
Arun <arunnn101010@gmail.com>
2026-04-05 00:20:28 +0530

    Add comments to containsDuplicate solution
    Added comments explaining the problem, approach, and complexity.

--- Array : Contains duplicate : Solution.py
+++ Array : Contains duplicate : Solution.py
@@ -1,1 +1,17 @@
+# Promblem - contains duplicate 
+# Appraoch - Hashset
+# Time and space complexit - O(n) & O(1)
+# Leetcode - 217class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
 
+        for(int num : nums) {
+            if(s.find(num) != s.end()) {
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+    }
+};
