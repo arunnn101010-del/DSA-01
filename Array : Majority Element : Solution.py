c134f0fc64237a3551c1b153dbf811f94cb56995
Arun <arunnn101010@gmail.com>
2026-04-04 17:56:12 +0530

    Add majority element solution using Moore's Voting Algorithm
    Implement Moore's Voting Algorithm to find the majority element in an array.

--- Array : Majority Element : Solution.py
+++ Array : Majority Element : Solution.py
@@ -1,1 +1,22 @@
+# promblem - Array Majority Elemnt 
+# Approach - Moore's Voting Algorithm
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 169
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans = 0;
 
+        for(int i=0; i < nums.size(); i++) {
+            if(freq == 0) {
+                ans = nums[i];
+            }
+            if(ans == nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
