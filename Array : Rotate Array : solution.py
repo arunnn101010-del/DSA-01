8e945670ce6fb1b618282c6229311887b30cfe92
Arun <arunnn101010@gmail.com>
2026-04-04 19:50:02 +0530

    Update Array : Rotate Array : solution.py

--- Array : Rotate Array : solution.py
+++ Array : Rotate Array : solution.py
@@ -1,1 +1,15 @@
+# Promblem - Rotate Array 
+# Approach - Reversal Algorithm 
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 189
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+      int n = nums.size();
+      k = k % n;
 
+      reverse(nums.begin(), nums.end());
+      reverse(nums.begin(), nums.begin() + k);
+      reverse(nums.begin() + k, nums.end());
+    }
+};
