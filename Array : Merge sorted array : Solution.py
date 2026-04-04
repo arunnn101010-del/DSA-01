0893afc1d41cc43e138a261f24fda7edb4ab0836
Arun <arunnn101010@gmail.com>
2026-04-04 14:04:20 +0530

    Add merge function for merging sorted arrays
    Implement merge function to combine two sorted arrays in-place using a reverse two-pointer approach.

--- Array : Merge sorted array : Solution.py
+++ Array : Merge sorted array : Solution.py
@@ -1,1 +1,22 @@
+# Promblem - Merge sorted array
+# Approach - Two pointers (reverse approach)
+# Time and space complexity - O(m+n) & O(1)
+# leetcode - 88
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int idx = m+n-1, i = m-1 , j = n-1;
 
+        while(i >= 0 && j >= 0) {
+            if(A[i] >= B[j]) {
+                A[idx--] = A[i--];
+            } else {
+                A[idx--] = B[j--];
+            }
+        }
+
+        while( j >= 0) {
+            A[idx--] = B[j--];
+        }
+    }
+};
