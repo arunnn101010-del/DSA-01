4341902b0d5e4f66b5eedf770d960e62f191c16d
Arun <arunnn101010@gmail.com>
2026-04-05 12:12:04 +0530

    Add missing number solution using sum formula
    Implement a solution for finding the missing number using the sum formula approach.

--- Array : Missing number : Solution.py
+++ Array : Missing number : Solution.py
@@ -1,1 +1,18 @@
+# Promblem - Missing Number 
+# Appraoch - Sum formula 
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 268
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
 
+        int total = n * (n+1) / 2;
+
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        return total - sum;
+    }
+};
