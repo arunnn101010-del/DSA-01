2f9b82ef525d62a80a7cf37271cce0ce88ff1af1
Arun <arunnn101010@gmail.com>
2026-04-05 23:48:30 +0530

    Add solution for Container with Most Water problem
    Implemented a solution for the 'Container with Most Water' problem using a two-pointer approach.

--- Array : Container with most water : solution.py
+++ Array : Container with most water : solution.py
@@ -1,1 +1,22 @@
+# Promblem - Container with most water
+# Approach - Two pointers
+# Time and space complexity - O(n) & O(1) 
+# Leetcode and Diffculty - 11 & Medium
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxWater = 0;
+        int lp = 0; int rp = height.size()-1;
 
+        while(lp < rp) {
+            int w = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            int currWater = w * ht;
+            maxWater = max(maxWater, currWater);
+
+            height[lp] < height[rp] ? lp++ : rp--;
+        }
+        
+        return maxWater;
+    }
+};
