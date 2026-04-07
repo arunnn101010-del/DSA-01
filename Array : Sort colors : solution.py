65389ce3fa9f9e1bf7d74293b19731bf06a35c1c
Arun <arunnn101010@gmail.com>
2026-04-07 23:13:52 +0530

    Update Array : Sort colors : solution.py

--- Array : Sort colors : solution.py
+++ Array : Sort colors : solution.py
@@ -1,1 +1,26 @@
+# Promblem - sort colors
+# approach - 3 pointers
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 75
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0;
+        int high = nums.size() - 1;
 
+        while(mid <= high ) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
