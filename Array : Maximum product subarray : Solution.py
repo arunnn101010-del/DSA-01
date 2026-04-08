2917a9c826eacc517fbf7b71b802c9d295ef3ed8
Arun <arunnn101010@gmail.com>
2026-04-08 23:36:21 +0530

    Update Array : Maximum product subarray : Solution.py

--- Array : Maximum product subarray : Solution.py
+++ Array : Maximum product subarray : Solution.py
@@ -1,1 +1,22 @@
+# Promblem - Maximum product subarray 
+# Approach - Dynamic programming 
+# Time and space complexity - O(n) & O(1)
+# Leetcode - 152
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int ans = nums[0];
 
+        for(int i = 1; i < nums.size(); i++) {
+            int temp = currMax;
+
+            currMax = max({nums[i], nums[i]*currMax, nums[i]*currMin});
+            currMin = min({nums[i], nums[i]*temp, nums[i]*currMin});
+
+            ans = max(ans, currMax);
+        }
+        return ans;
+    }
+};
