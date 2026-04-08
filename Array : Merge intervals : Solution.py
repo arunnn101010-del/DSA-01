b37a8add4ce56c6cd8a5ecae6213021c919fad2f
Arun <arunnn101010@gmail.com>
2026-04-08 23:42:03 +0530

    Add merge intervals solution in Solution.py
    Implemented a solution to merge overlapping intervals by sorting and iterating through the list.

--- Array : Merge intervals : Solution.py
+++ Array : Merge intervals : Solution.py
@@ -1,1 +1,23 @@
+# promblem - merge intervals 
+# Approach - Merge + sort
+# Time and space complexity - O(n log n) & O(n)
+# Leetcode and difficulty level - 56 and medium
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
 
+        sort(intervals.begin(), intervals.end());
+
+        ans.push_back(intervals[0]);
+
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
