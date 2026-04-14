dc8d252d68a2c6f826fa03d7d2d7598c83dbb674
Arun <arunnn101010@gmail.com>
2026-04-14 19:30:08 +0530

    Update Array : Find peak element : solution.py

--- Array : Find peak element : solution.py
+++ Array : Find peak element : solution.py
@@ -1,1 +1,21 @@
+# Promblem - Find peak element
+# Approach - Binary serach 
+# Space and space complexity - O(log n) & O(1)
+# Leetcode and diffculty level - 162 & medium 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l=0, r=nums.size()-1;
 
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] < nums[mid+1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
