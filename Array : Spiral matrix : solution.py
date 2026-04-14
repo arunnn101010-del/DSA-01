b2469460c2c7871f06caa5c6bd02db5b88896148
Arun <arunnn101010@gmail.com>
2026-04-14 19:23:42 +0530

    Update Array : Spiral matrix : solution.py

--- Array : Spiral matrix : solution.py
+++ Array : Spiral matrix : solution.py
@@ -1,1 +1,43 @@
+# Promblem - Spiral matrix 
+# Approach - Pointers 
+# Time and space complexity - O(m*n) & O(1)
+# Leetcode and diffculty level - 54 & medium
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int srow = 0, scol = 0, erow = m-1, ecol = n-1;
+        vector<int>ans;
 
+        while(srow <= erow && scol <= ecol) {
+            //top
+            for(int j=scol; j<=ecol; j++) {
+                ans.push_back(mat[srow][j]);
+            }
+
+            //right
+            for(int i=srow+1; i<=erow; i++) {
+                ans.push_back(mat[i][ecol]);
+            }
+
+            //bottom
+            for(int j=ecol-1; j>=scol; j--) {
+                if(srow == erow) {
+                    break;
+                }
+                ans.push_back(mat[erow][j]);
+            }
+
+            //left
+            for(int i=erow-1; i>=srow+1; i--) {
+                if(scol == ecol) {
+                    break;
+                }
+                ans.push_back(mat[i][scol]);
+            }
+            srow++; erow--; scol++; ecol--;
+        }
+
+        return ans;
+    }
+};
