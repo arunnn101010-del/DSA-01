84df0df3307c8c5327808bb41fdf0e9ab994d286
Arun <arunnn101010@gmail.com>
2026-04-16 23:09:25 +0530

    Implement image rotation using transpose and reverse

--- Array : Rotate image : solution.py
+++ Array : Rotate image : solution.py
@@ -2,3 +2,18 @@
 # Approach - transpose + reverse 
 # Time and space complexity - O(n^2) & O(1)
 # Leetcode and diffculty level - 48 & medium 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0; i<n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
