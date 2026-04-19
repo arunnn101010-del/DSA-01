1026d119b9c0bde594ddb0fdbb58f4b92940d5db
Arun <arunnn101010@gmail.com>
2026-04-19 13:53:32 +0530

    Add solution for Trapping Rain Water problem
    Implemented a solution for the Trapping Rain Water problem using a two-pointer approach with O(n) time and O(1) space complexity.

--- Array : Trapping rain water : solution.py
+++ Array : Trapping rain water : solution.py
@@ -1,1 +1,26 @@
+# Promblem - Trapping rain water 
+# Appraoch - two pointers
+# Time and space complexity - O(n) & O(1)
+# Leetcode and diffculty level - 42 & hard
+class solution {
+public:
+    int trap(vector<int>& height) {
+    int n = height.size();
+    int l = 0, r = n-1;
+    int lmax = 0, rmax = 0;
 
+    while(l < r) {
+        lmax = max(lmax, heght[l]);
+        rmax = max(rmax, height[r]);
+
+        if( lmax < rmax) {
+            ans += lmax - height[l];
+            l++; 
+        } else {
+            ans += rmax - height[r];
+            r--; 
+        }
+    }
+    return ans;
+  } 
+};
